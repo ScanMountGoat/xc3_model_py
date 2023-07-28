0317e59d42a40012f74d6a2fa89d6bafdffbd06e
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-07-28 13:11:47 -0500

    add type hints

---
+++ xc3_model_py/__init__.pyi
@@ -0,0 +1,122 @@
+from typing import List, Optional, ClassVar
+import numpy
+
+
+def load_model(wimdo_path: str) -> ModelRoot: ...
+
+
+def load_map(wismhd: str) -> List[ModelRoot]: ...
+
+
+class ModelRoot:
+    groups: List[ModelGroup]
+    image_textures: List[ImageTexture]
+
+
+class ModelGroup:
+    models: List[Model]
+    materials: List[Material]
+    skeleton: Optional[Skeleton]
+
+
+class Model:
+    meshes: List[Mesh]
+    vertex_buffers: List[VertexBuffer]
+    index_buffers: List[IndexBuffer]
+    instances: numpy.ndarray
+
+
+class Mesh:
+    vertex_buffer_index: int
+    index_buffer_index: int
+    material_index: int
+
+
+class Skeleton:
+    bones: List[Bone]
+
+
+class Bone:
+    name: str
+    transform: numpy.ndarray
+    parent_index: Optional[int]
+
+
+class Material:
+    name: str
+    textures: List[Texture]
+    shader: Optional[Shader]
+
+
+class Shader:
+    output_dependencies: dict[str, List[str]]
+
+
+class Texture:
+    image_texture_index: int
+
+
+class VertexBuffer:
+    attributes: List[AttributeData]
+    influences: List[Influence]
+
+
+class AttributeData:
+    attribute_type: AttributeType
+    data: numpy.ndarray
+
+
+class AttributeType:
+    Position: ClassVar[AttributeType]
+    Normal: ClassVar[AttributeType]
+    Tangent: ClassVar[AttributeType]
+    Uv1: ClassVar[AttributeType]
+    Uv2: ClassVar[AttributeType]
+    VertexColor: ClassVar[AttributeType]
+    WeightIndex: ClassVar[AttributeType]
+    SkinWeights: ClassVar[AttributeType]
+    BoneIndices: ClassVar[AttributeType]
+
+
+class Influence:
+    bone_name: str
+    weights: List[SkinWeight]
+
+
+class SkinWeight:
+    vertex_index: int
+    weight: float
+
+
+class IndexBuffer:
+    indices: numpy.ndarray
+
+
+class ImageTexture:
+    name: Optional[str]
+    width: int
+    height: int
+    depth: int
+    view_dimension: ViewDimension
+    image_format: ImageFormat
+    mipmap_count: int
+    image_data: bytes
+
+
+class ViewDimension:
+    D2: ClassVar[ViewDimension]
+    D3: ClassVar[ViewDimension]
+    Cube: ClassVar[ViewDimension]
+
+
+class ImageFormat:
+    R8Unorm: ClassVar[ImageFormat]
+    R8G8B8A8Unorm: ClassVar[ImageFormat]
+    R16G16B16A16Float: ClassVar[ImageFormat]
+    BC1Unorm: ClassVar[ImageFormat]
+    BC2Unorm: ClassVar[ImageFormat]
+    BC3Unorm: ClassVar[ImageFormat]
+    BC4Unorm: ClassVar[ImageFormat]
+    BC5Unorm: ClassVar[ImageFormat]
+    BC7Unorm: ClassVar[ImageFormat]
+    B8G8R8A8Unorm: ClassVar[ImageFormat]
